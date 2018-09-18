53e849b4ea24e16a5cefc77f6daff46a2466f5b4
Johan Bäckman <johan.c.backman@gmail.com>
2018-09-19 00:34:37 +0200

    Add group sorting when <= 5 man groups
    also add the unlocking of width/height slider to this addon rather than use a separate addon for it

--- LucyRaidFrames.lua
+++ LucyRaidFrames.lua
@@ -1,8 +1,14 @@
+--This is how you make the raid frames even more resizable :) you can also put all this in one line and just use a macro in game.
+local n,w,h="CompactUnitFrameProfilesGeneralOptionsFrame" h,w=
+_G[n.."HeightSlider"],
+_G[n.."WidthSlider"]
+h:SetMinMaxValues(1,150)
+w:SetMinMaxValues(1,150)
+
 hooksecurefunc("CompactUnitFrame_SetMaxBuffs", function(frame,numbuffs)
 
   local buffscale = 1.25;
   local debuffscale = 1.1;
-  local maxbuffs = 3;         -- default: 3;
 
   for i=1, #frame.buffFrames do
     frame.buffFrames[i]:SetScale(buffscale);
@@ -12,7 +18,6 @@ hooksecurefunc("CompactUnitFrame_SetMaxBuffs", function(frame,numbuffs)
     frame.debuffFrames[i]:SetScale(debuffscale);
   end
 
-  frame.maxBuffs = maxbuffs;
 end);
 
 hooksecurefunc("CompactUnitFrame_UpdateName", function(frame)
@@ -29,10 +34,8 @@ hooksecurefunc("CompactUnitFrame_UpdateName", function(frame)
 
   if InCombatLockdown() then
     name:SetAlpha(0.15);
-    -- name:Hide(1);
   else
     name:SetAlpha(1);
-    -- name:Show();
   end
 
 end);
@@ -55,3 +58,37 @@ hooksecurefunc("CompactUnitFrame_UpdateRoleIcon", function(frame)
   end
 
 end);
+
+local function sortCompactRaidFrameContainer(frame)
+
+  if ( not frame.groupMode) then
+    return;
+  end
+
+  LoadAddOn("CompactRaidFrameContainer");
+
+  local amountUnitFrames = frame:GetNumChildren();
+  print("Updating raid frames, should it sort? Amount frames: " .. amountUnitFrames);
+  if (amountUnitFrames < 6) then
+
+    local Sort_GroupAscending_PlayerBottom = function(t1, t2)
+      if UnitIsUnit(t1, "player") then
+        return false;
+      elseif UnitIsUnit(t2, "player") then
+        return true;
+      else
+        return t1 < t2;
+      end
+    end
+
+    CompactRaidFrameContainer_SetFlowSortFunction(frame, Sort_GroupAscending_PlayerBottom);
+    CompactRaidFrameContainer_TryUpdate(frame);
+  end
+
+
+end
+
+hooksecurefunc("CompactRaidFrameContainer_UpdateDisplayedUnits", function(frame)
+  sortCompactRaidFrameContainer(frame);
+end);
+
